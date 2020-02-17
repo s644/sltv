1e12eb9ad3e2246f1da54f4275e282ee50e096b5
s644 <spidersoft@gelsennet.de>
2020-02-18 00:30:59 +0100

    [feature] unread message count in doc title
    display unread message count and unread message with @username count in document title if document visibility is hidden

--- stvl_simple_chat_enhancer.user.js
+++ stvl_simple_chat_enhancer.user.js
@@ -1,15 +1,22 @@
 // ==UserScript==
 // @name         [Skylinetv.live] Simple chat enhancer
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Simple chat enhancement with @userhandle support, the ability to click on usernames for easy address and clickable urls
 // @author       Arno_Nuehm
 // @match        https://skylinetv.live/dabei/
 // @license      https://creativecommons.org/licenses/by-nc-sa/4.0/
+// @updateURL    https://github.com/s644/sltv/raw/master/stvl_simple_chat_enhancer.user.js
+// @supportURL   https://github.com/s644/sltv/issues
 // ==/UserScript==
 
 (function() {
     //'use strict';
+
+    var unread = 0;
+    var unreadPriority = 0;
+    var origTitle = document.title;
+
 
     var urlRegex = /((?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?((?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2}))|((\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(\.(\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?)/
     // get nick name
@@ -49,6 +56,11 @@
                         // wrap text nodes in span container
                         var wrapNode = document.createElement('span');
 
+                        // check for priority messages
+                        if(~node.data.indexOf("@" + user)) {
+                            unreadPriority++;
+                        }
+
                         // highlight my user name
                         wrapNode.innerHTML = node.data.replace("@" + user, '<span class="badge premium">' + user + '</span>').replace(urlRegex,"<a href=\"$1\" target=\"_blank\">$2</a>");
                         msg.appendChild(wrapNode);
@@ -59,6 +71,13 @@
 
                 // append our message container
                 mutation.target.appendChild(msg);
+
+                if(document.visibilityState == "hidden") {
+                    unread++;
+                    document.title = "(" + unread.toString() + (unreadPriority > 0 ? ("|*" + unreadPriority.toString()):"") + ") - " + origTitle;
+                } else {
+                    unreadPriority = 0;
+                }
             }
         });
     });
@@ -81,4 +100,16 @@
         selection.removeAllRanges();
         selection.addRange(range);
     }
+
+    function focusChanged() {
+        // handle focus changes
+        if (document.visibilityState !== "hidden") {
+            unread = 0;
+            unreadPriority = 0;
+            document.title = origTitle;
+        }
+    }
+
+    document.addEventListener('visibilitychange', focusChanged, false);
+
 })();
